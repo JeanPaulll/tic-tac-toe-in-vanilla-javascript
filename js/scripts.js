6da08358b47378ba52ca50a496d388cc76317658
Jean Paul <jeanpaulwebb@gmail.com>
2023-07-18 00:36:25 -0300

    :sparkles: Declare the winner and update the scoreboard

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,29 @@ function checkEl(player1, player2) {
   else el = o;
   return el;
 }
+
+/**
+ * @description
+ * Declara o vencedor e atualiza o placar
+ */
+function declareWinner(winner) {
+  let scoreBoardX = document.querySelector("#scoreboard-1");
+  let scoreBoardO = document.querySelector("#scoreboard-2");
+  let message = "";
+  if (winner == "x") {
+    scoreBoardX.textContent = parseInt(scoreBoardX.textContent) + 1;
+    message = "O Jogador 1 venceu!";
+  } else if (winner == "o") {
+    scoreBoardO.textContent = parseInt(scoreBoardO.textContent) + 1;
+    message = "O Jogador 2 venceu!";
+  } else {
+    message = "Deu velha!";
+  }
+  messageText.innerHTML = message;
+  messageText.classList.remove("hide");
+  messageContainer.classList.remove("hide");
+  setTimeout(() => {
+    messageContainer.classList.add("hide");
+  }, 4000);
+  cleanBoard();
+}
