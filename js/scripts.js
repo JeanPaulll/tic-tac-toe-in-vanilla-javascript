38318c1f42ef3abff3c1fd099d77adfd7014071b
Jean Paul <jeanpaulwebb@gmail.com>
2023-07-18 00:34:21 -0300

    :sparkles: creating styles and js files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
